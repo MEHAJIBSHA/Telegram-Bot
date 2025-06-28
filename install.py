e114ba256d2cd31c7c1019b517cc4de8af4c1fd5
MEHAJIBSHA <mehajibsha1998@gmail.com>
2025-06-28 07:50:19 +0530

    Add files via upload

--- install.py
+++ install.py
@@ -1,45 +1,21 @@
-import sys, time, os
+# Credits to Pranav
 
 
-def m(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.1)
+import asyncio
+from pyrogram import Client
 
-os.system("clear")
-print("""
-      ██████╗░░█████╗░██████╗░██╗░░██╗
-      ██╔══██╗██╔══██╗██╔══██╗██║░██╔╝
-      ██║░░██║███████║██████╔╝█████═╝░
-      ██║░░██║██╔══██║██╔══██╗██╔═██╗░
-      ██████╔╝██║░░██║██║░░██║██║░╚██╗
-      ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝
 
-      ██████╗░░█████╗░████████╗
-      ██╔══██╗██╔══██╗╚══██╔══╝
-      ██████╦╝██║░░██║░░░██║░░░
-      ██╔══██╗██║░░██║░░░██║░░░
-      ██████╦╝╚█████╔╝░░░██║░░░
-      ╚═════╝░░╚════╝░░░░╚═╝░░░
-      ░ ░\x1b[00m\033[041m TERMUX TELEGRAM BOT MrDevils  \033[00m\x1b[1;00m░░
-        ░ ░   ░   ░    ░ ░   ░    ░   ░   ░\x1b[00m
-""")
-m('\x1b[00m\033[041m Install bahan automatis  \033[00m')
-m('\x1b[00m\033[041m Jangan Keluar Dari Termux Sebelum Selesai Menginstall!!  \033[00m')
-os.system("pkg update -y")
-os.system("pkg upgrade -y")
-os.system("pkg install nano")
-os.system("pkg install python -y")
-os.system("pkg install python2 -y")
-os.system("pkg install nodejs -y")
-os.system("pkg install libwebp -y")
-os.system("pkg install ffmpeg -y")
-os.system("npm install")
-os.system("npm audit fix")
-os.system("npm install axios")
-os.system("npm install telegraf@3.32")
-m('\x1b[00m\033[041m Memulai Telegram Bot...  \033[00m')
-os.system("clear")
-os.system("python start.py")
-m("DONE")
+async def generate_and_send_string_session(api_id, api_hash):
+    async with Client("my_account", api_id=api_id, api_hash=api_hash) as app:
+        string_session = await app.export_session_string()
+        await app.send_message(
+            "me",
+            f"<b>Your Pyrogram String Session:</b>\n\n<code>{string_session}</code>",
+        )
+        print("String session has been sent to your 'Saved Messages'.")
+
+
+if __name__ == "__main__":
+    api_id = int(input("27934630: "))
+    api_hash = input("5fbce12d70c2bdedf8b1e549e8ae818b: ")
+    asyncio.run(generate_and_send_string_session(api_id, api_hash))
